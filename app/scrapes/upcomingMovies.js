f3a2817909cb67164d12d509880d99f03baee8b3
Evan Tucker <eej.tucker@gmail.com>
2018-03-14 09:39:23 -0600

    worked on scraping

--- app/scrapes/upcomingMovies.js
+++ app/scrapes/upcomingMovies.js
@@ -4,9 +4,15 @@ const cheerio = require('cheerio');
 
 const upcomingMoives = ()=> {
     axios.get('https://www.fandango.com/moviescomingsoon')
-    .then((response)=> {
-        console.log(response); 
-        let urlData = response;
+    .then((res)=> {
+        const $ = cheerio.load(res.data);
+        let urlData = res;
+        // console.log($('ul.visual-list.movie-list').children('li'));
+        // console.log($('div.visual-detail'));
+        $('div.visual-detail').each((i, elem)=> {
+            console.log($(this).children('span.visual-sub-title'));
+            console.log('------------------------');
+        })
     }).catch((err)=> {
         console.log(err);
     });
