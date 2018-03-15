b050d0ef7404c524092b32b740ac939306c72981
Evan Tucker <eej.tucker@gmail.com>
2018-03-14 20:11:31 -0600

    still struggling with scraping

--- app/scrapes/upcomingMovies.js
+++ app/scrapes/upcomingMovies.js
@@ -3,16 +3,13 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const upcomingMoives = ()=> {
-    axios.get('https://www.fandango.com/moviescomingsoon')
+    axios.get('https://www.rottentomatoes.com/browse/in-theaters/')
     .then((res)=> {
-        const $ = cheerio.load(res.data);
-        let urlData = res;
-        // console.log($('ul.visual-list.movie-list').children('li'));
-        // console.log($('div.visual-detail'));
-        $('div.visual-detail').each((i, elem)=> {
-            console.log($(this).children('span.visual-sub-title'));
-            console.log('------------------------');
+        var $ = cheerio.load(res.data);
+        $('ul.nav.nav-pils.nav-stacked.navigator-menu').children().each((i, element)=>{
+            console.log('working')
         })
+        // console.log($('ul.nav.nav-pils.nav-stacked.navigator-menu').children());
     }).catch((err)=> {
         console.log(err);
     });
